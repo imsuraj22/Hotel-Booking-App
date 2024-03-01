204780716252abd567eec027ec64511a75df9fab
Suraj <surajgaikwad0287@gmail.com>
2024-03-01 21:33:26 +0530

    added admin

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -4,19 +4,21 @@ import {createError} from "../utils/error.js"
 import jwt from 'jsonwebtoken';
 
 export const register=async (req,res,next)=>{
+    
     try {
         const salt=bcrypt.genSaltSync(10);
+        
         const hash=bcrypt.hashSync(req.body.password,salt);
         const newUser=new User({
-            username:req.body.username,
-            email:req.body.email,
+            ...req.body,
             password:hash,
         })
 
         await newUser.save();
         res.status(200).send("User has been created")
     } catch (error) {
-        next(error)
+        // console.log("entered in register")
+        next("entered in error",error)
     }
 }
 
@@ -38,7 +40,7 @@ export const login= async (req,res,next)=>{
        const {password,isAdmin,...otherDetails}=user._doc;
        res.cookie("access_token",token,{
         httpOnly:true,
-       }).status(200).json({...otherDetails})
+       }).status(200).json({ details:{...otherDetails},isAdmin})
     } catch (error) {
         next(error)
     }
